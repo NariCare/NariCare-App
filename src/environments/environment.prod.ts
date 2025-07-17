78783515eeb5d4ad4b47c9fa6cd7dd1facecca00
Davinder Singh <19davinder@gmail.com>
2025-07-17 17:52:17 +0530

    fine for now

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC8d-hqyjz-cPO9O2srlFp0UCWivB-4T_s",
+    authDomain: "naricare-afb05.firebaseapp.com",
+    projectId: "naricare-afb05",
+    storageBucket: "naricare-afb05.firebasestorage.app",
+    messagingSenderId: "367456865327",
+    appId: "1:367456865327:web:b68a3d7914199803658ada",
+    measurementId: "G-PYV60K3B2D"
+  },
+  openaiApiKey: "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"
+};
