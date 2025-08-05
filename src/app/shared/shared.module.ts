e7dbac4ba2aa9b2ff2b2492018aa4ed1c193dcd6
Davinder Singh <19davinder@gmail.com>
2025-08-05 18:17:06 +0530

    Implement Consultation Booking System

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,24 +1,29 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
+import { ReactiveFormsModule } from '@angular/forms';
 import { VideoPlayerModalComponent } from '../components/video-player-modal/video-player-modal.component';
 import { TimelineModalComponent } from '../components/timeline-modal/timeline-modal.component';
 import { SpecificWeekModalComponent } from '../components/specific-week-modal/specific-week-modal.component';
+import { ConsultationBookingModalComponent } from '../components/consultation-booking-modal/consultation-booking-modal.component';
 
 @NgModule({
   declarations: [
     VideoPlayerModalComponent,
     TimelineModalComponent,
-    SpecificWeekModalComponent
+    SpecificWeekModalComponent,
+    ConsultationBookingModalComponent
   ],
   imports: [
     CommonModule,
-    IonicModule
+    IonicModule,
+    ReactiveFormsModule
   ],
   exports: [
     VideoPlayerModalComponent,
     TimelineModalComponent,
-    SpecificWeekModalComponent
+    SpecificWeekModalComponent,
+    ConsultationBookingModalComponent
   ]
 })
 export class SharedModule { }
